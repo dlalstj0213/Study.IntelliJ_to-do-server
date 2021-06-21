ff5b6280bbffe7f16af7c311f6257ede1f3018f1
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-22 00:02:12 +0900

    Modify Application.java

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.example.repository.TodoRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("SYSTEM RUN: HELLO TO-DO");
-
-        //TodoRepository repository;
-
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 }
