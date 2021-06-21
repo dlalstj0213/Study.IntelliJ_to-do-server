8674392e4b5e678a0e10c82939d105034896a0dc
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-21 23:08:24 +0900

    add repository

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("SYSTEM RUN");
+        System.out.println("SYSTEM RUN: HELLO TO-DO");
+
+        //TodoRepository repository;
+
     }
 }
